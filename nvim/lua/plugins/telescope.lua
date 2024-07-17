17bb7e31e89e19942030d214893683b0f8dd0a86
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-07-17 16:58:15 +0700

    git commit -m "feat(telescope): Add telescope-live-grep-args extension
    - Added `telescope-live-grep-args.nvim` to the plugin dependencies in `lazy-lock.json`.
    - Updated `telescope.lua` to include the `telescope-live-grep-args` extension with configuration for auto-quoting in `live_grep_args`.
    - Updated `plenary.nvim` to a compatible version and set the `telescope.nvim` plugin tag to `0.1.8` for stability.
    
    This update integrates the `telescope-live-grep-args` extension for improved live grep capabilities and ensures compatibility with existing plugins."

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,16 +1,46 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+  },
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+
+    telescope.setup({
       defaults = {
         preview = false
+      },
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = {         -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              -- freeze the current list and start a fuzzy search in the frozen list
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        }
       }
     })
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+    telescope.load_extension("live_grep_args")
 
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fs", require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
   end
 }
