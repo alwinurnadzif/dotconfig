45ae834e3d7e7d07f18de72d8c260dd3a2119bc0
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-08-18 15:35:04 +0700

    feat(neo-tree): configure to show hidden files and follow current file
    - Updated `neo-tree` configuration to show dotfiles by setting `hide_dotfiles` to false.
    - Enabled `follow_current_file` to keep the current file in view and to leave directories open.
    - Added configuration for buffer settings to follow the current file.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,4 +6,23 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
+  config = function()
+    require("neo-tree").setup({
+      buffers = {
+        follow_current_file = {
+          enabled = true
+        }
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false
+        },
+
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+      }
+    })
+  end
 }
