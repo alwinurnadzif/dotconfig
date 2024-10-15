84dbfb2464d82750564d5542fd9cf8a09e22ba53
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-10-15 14:08:18 +0700

    feat: treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "vue", "go", "javascript" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+      configs.setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "vue", "go", "javascript" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    enable = true
+  }
 }
