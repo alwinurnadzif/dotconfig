b9a12c427feca0079918eae48e15b285d75611ee
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-12-09 22:21:43 +0700

    something

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,11 +10,25 @@ return {
         ensure_installed = { "lua", "vim", "vimdoc", "vue", "go", "javascript" },
         highlight = { enable = true },
         indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,           -- Automatically jump forward to text object
+            keymaps = {
+              ["af"] = "@function.outer", -- Select the whole function
+              ["if"] = "@function.inner", -- Select inside the function
+            },
+          },
+        }
       })
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
     enable = true
-  }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
