7a17e540f80129b47fab3812e8b0ab58f1ded97a
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-12-11 00:13:57 +0700

    refactor(lsp): simplify TypeScript and Vue LSP configurations
    - Removed custom TypeScript plugin configuration from `ts_ls` setup.
    - Simplified `ts_ls` filetypes by excluding Vue files.
    - Replaced `volar` LSP setup with `vuels` for Vue file support.
    - Removed dynamic TypeScript server path resolution to streamline configuration.
    
    This change improves maintainability by reducing complexity and ensuring a clearer separation of responsibilities between `ts_ls` and `vuels`.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -57,46 +57,18 @@ return {
         },
       })
 
-      lspconfig.ts_ls.setup({
+      require 'lspconfig'.ts_ls.setup {
         init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = "/home/tokio/workspace/apps/node/lib/node_modules/@vue/typescript-plugin",
-              languages = { "javascript", "typescript", "vue" },
-            },
-          },
+          plugins = {},
         },
         filetypes = {
           "javascript",
           "typescript",
-          "vue",
         },
-      })
-      local util = require 'lspconfig.util'
-      local function get_typescript_server_path(root_dir)
-        local global_ts = '/home/tokio/workspace/apps/node/lib/node_modules/typescript/lib'
-        -- Alternative location if installed as root:
-        -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-        local found_ts = ''
-        local function check_dir(path)
-          found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
-          if util.path.exists(found_ts) then
-            return path
-          end
-        end
-        if util.search_ancestors(root_dir, check_dir) then
-          return found_ts
-        else
-          return global_ts
-        end
-      end
+      }
 
-      require 'lspconfig'.volar.setup {
-        capabilities = capabilities,
-        on_new_config = function(new_config, new_root_dir)
-          new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-        end,
+      require 'lspconfig'.vuels.setup {
+        capabilities = capabilities
       }
 
       lspconfig.emmet_ls.setup({
