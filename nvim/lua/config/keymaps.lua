456d83cacc76434247893e4356d9a4dab870bdac
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2025-05-31 13:21:53 +0700

    ref(nvim): keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -18,8 +18,8 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 vim.keymap.set("n", "t", "`", { noremap = true, silent = true })
 
 
-vim.keymap.set("v", "y", '"+y')
-vim.keymap.set("n", "y", '"+y')
+vim.keymap.set("v", "Y", '"+y')
+vim.keymap.set("n", "Y", '"+y')
 
-vim.keymap.set("v", "p", '"+p')
-vim.keymap.set("n", "p", '"+p')
+vim.keymap.set("v", "P", '"+p')
+vim.keymap.set("n", "P", '"+p')
